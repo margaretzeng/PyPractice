7b58e64d64d159a7f0025650aa8f69d157ba5be4
margaretzeng <margaret.22mz@gmail.com>
2021-07-13 03:20:27 -0500

    Create Chapter3-4.py

---
+++ Chapter3-4.py
@@ -0,0 +1,46 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jul 13 02:08:04 2021
+
+@author: margaret
+"""
+
+
+import pandas as pd
+
+long_df = pd.read_csv(
+    './data/long_Data.csv',
+    usecols = ['date', 'datatype', 'value']
+    ).rename(columns = {'value': 'temp_C'}).assign(
+        date = lambda x:pd.to_datetime(x.date),
+        temp_F = lambda x: (x.temp_C * 9/5) +32)
+
+long_df.set_index('date').head(6).T
+
+
+long_df
+
+pivoted_df = long_df.pivot(index='date', columns='datatype', values='temp_C')
+
+pivoted_df.head()
+
+pivoted_df.describe()
+
+pivoted_df = long_df.pivot(
+    index = 'date', columns = 'datatype',
+    values= ['temp_C', 'temp_F']
+)
+
+pivoted_df.describe()
+
+pivoted_df['temp_F']['TMIN'].head()
+
+multi_index_df = long_df.set_index(['date', 'datatype'])
+
+multi_index_df.head().index
+
+multi_index_df.head()
+
+unstacked_df = multi_index_df.unstack()
+
+unstacked_df.head()
